97ef65c1a5aa61f5ae9e4abba1c055bd7e9e924a
Pritam <pritam.sarkar@medlife.com>
2020-07-05 03:54:56 +0530

    Add spring boot skeleton

---
+++ src/test/java/com/atx/core/hera/HeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atx.core.hera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
