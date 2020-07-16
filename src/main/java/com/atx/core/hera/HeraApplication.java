bc20fb96f762c2093498346e4e35d7036cc5c1e4
Pritam <pritam.sarkar@medlife.com>
2020-07-17 00:46:21 +0530

    Add axon, h2, springfox setup

--- src/main/java/com/atx/core/hera/HeraApplication.java
+++ src/main/java/com/atx/core/hera/HeraApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HeraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HeraApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HeraApplication.class, args);
+    }
 
 }
